f3a4b325077faaeeac641ad0696fdd3862402011
dongwei <ibaymax@outlook.com>
2021-11-29 09:21:09 +0800

    merge array

--- src/main/java/com/dong/basic/MergeList.java
+++ src/main/java/com/dong/basic/MergeList.java
@@ -2,5 +2,15 @@ package com.dong.basic;
 
 public class MergeList {
     public void merge(int A[], int m, int B[], int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        // start from end
+        while (i >= 0 && j >= 0) {
+            A[k--] = A[i] > B[j] ? A[i--] : B[j--];
+        }
+        while (j >= 0) {
+            A[k--] = B[j--];
+        }
     }
 }
